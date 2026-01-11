a9a39fbd75f87719d948cde4221a6fb3db590cd5
Fajar Setiawan <21102183@ittelkom-pwt.ac.id>
2026-01-11 15:43:47 +0700

    Waktunya TURU

--- app/Http/Controllers/AbsenController.php
+++ app/Http/Controllers/AbsenController.php
@@ -37,25 +37,25 @@ class AbsenController extends Controller
 
         $pengajar = Auth::user();
 
-    // ambil semua mapel_id yang diampu pengajar login
-    $mapelIds = $pengajar->mengajar()
-        ->select('mapels.id') // kasih prefix biar jelas
-        ->pluck('id');
+        // ambil semua mapel_id yang diampu pengajar login
+        $mapelIds = $pengajar->mengajar()
+            ->select('mapels.id') // kasih prefix biar jelas
+            ->pluck('id');
 
-    // pastikan mapel ini diampu pengajar
-    if (!in_array($mapel_id, $mapelIds->toArray())) {
-        abort(403, 'Kamu tidak mengampu mapel ini.');
-    }
+        // pastikan mapel ini diampu pengajar
+        if (!in_array($mapel_id, $mapelIds->toArray())) {
+            abort(403, 'Kamu tidak mengampu mapel ini.');
+        }
 
-    // ambil siswa yang kelasnya sama dan mengambil mapel tersebut
-    $siswas = Siswa::where('kelas_id', $kelas_id)
-        ->whereHas('mengambil', function ($q) use ($mapel_id) {
-            $q->where('mapels.id', $mapel_id); // prefix jelas biar ga ambigu
-        })->orderBy('nama', 'asc')->get();
-        $kelas = Kelas::select('kelas')->where('id' ,'=', $kelas_id)->get();
+        // ambil siswa yang kelasnya sama dan mengambil mapel tersebut
+        $siswas = Siswa::where('kelas_id', $kelas_id)
+            ->whereHas('mengambil', function ($q) use ($mapel_id) {
+                $q->where('mapels.id', $mapel_id); // prefix jelas biar ga ambigu
+            })->orderBy('nama', 'asc')->get();
+        $kelas = Kelas::select('kelas')->where('id', '=', $kelas_id)->get();
         $mapel = Mapel::where('id', '=', $mapel_id)->get();
         $temporarySiswaIds = TemporaryNilai::where('user_id', $pengajar)->pluck('siswa_id')->toArray();
-        return view('user.absentCreate', ['header' => 'Absence from ', 'siswa' => $siswas , 'kelas' => $kelas, 'mapel' => $mapel, 'temporarySiswaIds' => $temporarySiswaIds,]);
+        return view('user.absentCreate', ['header' => 'Absence from ', 'siswa' => $siswas, 'kelas' => $kelas, 'mapel' => $mapel, 'temporarySiswaIds' => $temporarySiswaIds,]);
     }
 
     public function Showmap(Kelas $kelas)
@@ -82,18 +82,19 @@ class AbsenController extends Controller
      * Store a newly created resource in storage.
      */
     //khusus SMA BANYUMAS
-    public function store1(Request $request){
+    public function store1(Request $request)
+    {
         $pengajar = Auth::id();
         $semuaSiswa = $request->input('all', []);
         $siswaHadir = $request->input('siswa', []);
         Validator::make($request->all(), [
-                'tanggal' => 'required',
-                'sumary'  => 'required|max:200',
-            ],[
-                'required' => ':attribute wajib diisi',
-                'max'      => ':attribute maksimal 200 karakter',
-            ])->validate();
-        if($request->action == 'save'){
+            'tanggal' => 'required',
+            'sumary'  => 'required|max:200',
+        ], [
+            'required' => ':attribute wajib diisi',
+            'max'      => ':attribute maksimal 200 karakter',
+        ])->validate();
+        if ($request->action == 'save') {
             foreach ($semuaSiswa as $siswaId) {
                 $status = in_array($siswaId, $siswaHadir) ? 'hadir' : 'tidak hadir';
                 TemporaryNilai::create([
@@ -106,7 +107,7 @@ class AbsenController extends Controller
                 ]);
             }
             return redirect('absent')->with('berhasil', 'Data absensi tersimpan sementara');
-        }else{
+        } else {
             foreach ($semuaSiswa as $siswaId) {
                 $status = in_array($siswaId, $siswaHadir) ? 'hadir' : 'tidak hadir';
                 Absensi::create([
@@ -118,16 +119,14 @@ class AbsenController extends Controller
                     'sumary'   => $request->sumary,
                 ]);
             }
-        $hapus = TemporaryNilai::where('user_id', '=', Auth::user()->id)->where('mapel_id', '=', 1)->delete();
-        
-        return redirect('absent')->with(['berhasil' => 'Absensi berhasil']);
+            $hapus = TemporaryNilai::where('user_id', '=', Auth::user()->id)->where('mapel_id', '=', $request->mapel)->delete();
+
+            return redirect('absent')->with(['berhasil' => 'Absensi berhasil']);
         }
     }
 
 
-    public function store2(Request $request){
-        
-    }
+    public function store2(Request $request) {}
 
     /**
      * Display the specified resource.
